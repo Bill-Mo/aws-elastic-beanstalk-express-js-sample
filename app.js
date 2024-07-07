27c9ac0bd8c51d740faf55d6c7dc8754d2cd5b22
bill0603 <2233925323@qq.com>
2024-07-07 19:47:58 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bills program!'));
+app.get('/', (req, res) => res.send('Please give me an offer!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
